8d89bef20f4da9bed6aa80a7b1cbf1d2c0a54c6b
anizozina <anizozina@gmail.com>
2025-02-01 11:58:30 +0900

    Initial commit

---
+++ test/redash-ecs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RedashEcs from '../lib/redash-ecs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/redash-ecs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RedashEcs.RedashEcsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
